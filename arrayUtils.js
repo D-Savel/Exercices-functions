a511c3bb6510bb20d8ea428bea98f80b8e274203
D-Savel <dad.savel@gmail.com>
2021-03-04 14:22:40 +0100

    Correction arrayUtils.js

--- arrayUtils.js
+++ arrayUtils.js
@@ -12,17 +12,22 @@ const biggest = (array) => {
 //Fonction qui classe les nombres d'un tableau dans l'ordre croissant en retournant un nouveau tableau
 //array corespond à un tableau de nombre de forme [nb, nb1, nb3, ...]
 const sortAscend = (array) => {
-  return array.sort()
+  return array.sort((a, b) => a - b)
 }
+
+console.log(`le plus grand nombre du tableau [99, 100, 101, 1] est ${biggest([99, 100, 101, 1])}`)
+
+
+console.log("\nVoici le tableau [99, 100, 101, 1] dans l'ordre croissant :")
+console.log(sortAscend([99, 100, 101, 1]))
 
 const makeUnique = (array) => {
-  return new Set([1, 2, 1, 3, 2, 4, 5, 7, 5, 1])
+  let newarray = []
+  for (const elem of array) {
+    !newarray.includes(elem) ? newarray.push(elem) : ''
+  }
+  return newarray
 }
 
-
-console.log(`le plus grand nombre du tableau [99, 100, 101, 1] est ${biggest([99, 100, 101, 1])}\n`)
-
-let sorted = sortAscend([99, 100, 101, 1])
-console.log("Voici le tableau [99, 100, 101, 1] dans l'ordre croissant :")
-console.log(sorted)
-console.log(makeUnique([1, 2, 1, 3, 2, 4, 5, 7, 5, 1]))
+console.log("\nVoici le tableau [1, 2, 8, 3, 8, 4, 5, 5, 5, 1] sans doublon triés dans l'ordre croissant :")
+console.log(sortAscend(makeUnique([1, 2, 8, 3, 8, 4, 5, 5, 5, 1])))
