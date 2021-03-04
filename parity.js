2c3b38bcac534b4571f4068711a370e40bfa6581
D-Savel <dad.savel@gmail.com>
2021-03-04 14:08:13 +0100

    correction parity.js

--- parity.js
+++ parity.js
@@ -1,19 +1,11 @@
 //Fonction qui prend pour argument un nombre(number) er retourne true s'il est impair
 const isOdd = (number) => {
-  if (number % 2 !== 0) {
-    return true
-  } else {
-    return false
-  }
+  return number % 2 === 0
 }
 console.log(`la fonction isOdd(5) retourne ${isOdd(5)}`)
 
 const isEven = (number) => {
-  if (isOdd(number) === false) {
-    return true
-  } else {
-    return false
-  }
+  return !isOdd(number)
 }
 
 console.log(`la fonction isEven(5) retourne ${isEven(5)}`)
